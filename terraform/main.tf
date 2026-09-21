eccb06812a6fcf9bbf6f7c604056a675b62bd5be
CL consulting <lcwolfconsulting@gmail.com>
2026-09-21 15:01:34 +0200

    feat: add Terraform CI pipeline for Codex Hunter

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,63 +1,27 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.0"
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.0"
     }
   }
 }
 
-provider "docker" {}
-
-# Réseau isolé pour Codex Hunter
-resource "docker_network" "codex_net" {
-  name = "codex_network"
-}
-
-# Image BDD PostgreSQL
-resource "docker_image" "postgres" {
-  name         = "postgres:15-alpine"
-  keep_locally = true
-}
-
-# Conteneur Base de Données (cartes & scans Pokémon)
-resource "docker_container" "db" {
-  image = docker_image.postgres.image_id
-  name  = "codex-db-tf"
-
-  networks_advanced {
-    name = docker_network.codex_net.name
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
+}
 
-  env = [
-    "POSTGRES_USER=${var.db_user}",
-    "POSTGRES_PASSWORD=${var.db_password}",
-    "POSTGRES_DB=${var.db_name}"
+resource "helm_release" "codex_hunter" {
+  name      = "codex-app-tf"
+  chart     = "../codex-chart"
+  namespace = "default"
+
+  values = [
+    <<-EOT
+    frontend:
+      replicaCount: 2
+    EOT
   ]
-
-  ports {
-    internal = 5432
-    external = 5432
-  }
-}
-
-# Image Frontend depuis GHCR
-resource "docker_image" "frontend_image" {
-  name         = var.frontend_image_name
-  keep_locally = true
-}
-
-# Conteneur Frontend (Interface Codex Hunter)
-resource "docker_container" "frontend" {
-  image = docker_image.frontend_image.image_id
-  name  = "codex-frontend-tf"
-
-  networks_advanced {
-    name = docker_network.codex_net.name
-  }
-
-  ports {
-    internal = 80
-    external = var.frontend_port
-  }
 }
